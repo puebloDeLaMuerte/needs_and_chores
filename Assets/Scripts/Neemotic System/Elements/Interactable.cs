f47a385a7e3869d34ae22c07821b8a6eef29a8d0
PuebloDeLaMuerte <pueblodelamuerte@schnabelbude.de>
2020-07-13 15:06:55 +0200

    bugfix: Multiple pickup interactions
    * multiple pickup interctions now possible (had to set "pickup repeat mode" to REPEAT in FPEInteractablePickup Script)
    * gameobject still gets rebased into "Don't destroy on loac". But functionality stays, because it only has to be within "Ineractables" GameObject at the start of the scene!

--- Assets/Scripts/Neemotic System/Elements/Interactable.cs
+++ Assets/Scripts/Neemotic System/Elements/Interactable.cs
@@ -4,54 +4,64 @@ using UnityEngine;
 
 namespace YBC.Neemotix
 {
-    class Interactable : MonoBehaviour
-    {
-        public Effect[] effects;
+	class Interactable : MonoBehaviour
+	{
+		public Effect[] effects;
 
-        private string myInteractableObjectName;
+		private string myInteractableObjectName;
+		private Transform myInteractableCollectionTransform;
 
-        private ChangeManager.PushEffectToQueue pushEffects;
-        public ChangeManager.PushEffectToQueue PushEffectsMethod {set => pushEffects = value; }
+		private ChangeManager.PushEffectToQueue pushEffects;
+		public ChangeManager.PushEffectToQueue PushEffectsMethod {set => pushEffects = value; }
 
-        public void Awake()
-        {
-            myInteractableObjectName = transform.name;
-        }
-        public void OnInteract()
-        {
-            foreach ( Effect e in effects )
-            {
-                pushEffects(e);
-            }
-            Debug.Log("You have just intercted, doesn't it feel good!");
-        }
+		public void Awake()
+		{
+			myInteractableObjectName = transform.name;
+			myInteractableCollectionTransform = transform.parent.transform;
+		}
+		public void OnInteract()
+		{
+			foreach ( Effect e in effects )
+			{
+				pushEffects(e);
+			}
 
-
-        public void OnInteractionStart()
-        {
-            foreach ( Effect e in effects )
-            {
-                pushEffects(e);
-            }
-            Debug.Log("You started interacting with " + myInteractableObjectName + ". Enjoy!");
-        }
+			Debug.Log("You have just intercted, doesn't it feel good!");
+		}
 
 
-        public void OnInteractionEnd()
-        {
-            foreach ( Effect e in effects )
-            {
-                if( e.durationInHours == 0)
-                {
-                    e.Revoke();
-                }
-            }
-            Debug.Log("You finished interacting with " + myInteractableObjectName  + ". Was it good?");
-        }
+		public void OnInteractionStart()
+		{
+			foreach ( Effect e in effects )
+			{
+				pushEffects(e);
+			}
+			Debug.Log("You started interacting with " + myInteractableObjectName + ". Enjoy!");
+		}
 
-        public string GetInteractableObjectName()
-        {
-            return myInteractableObjectName;
-        }
-    }
+
+		public void OnInteractionEnd()
+		{
+			foreach ( Effect e in effects )
+			{
+				if( e.durationInHours == 0)
+				{
+					e.Revoke();
+				}
+			}
+			Debug.Log("You finished interacting with " + myInteractableObjectName  + ". Was it good?");
+
+		}
+
+		public string GetInteractableObjectName()
+		{
+			return myInteractableObjectName;
+		}
+
+
+		public void RebaseGameobject()
+		{
+			transform.SetParent(myInteractableCollectionTransform);
+		}
+	}
 }
